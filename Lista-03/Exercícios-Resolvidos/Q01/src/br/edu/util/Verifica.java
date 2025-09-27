952339ae1cee6ee4edea83bd9b9584bbb310a95b
Erik13639 <barbosa.araujo63@aluno.ifce.edu.br>
2025-09-27 10:01:17 -0300

    Add files via upload

---
+++ Lista-03/Exercícios-Resolvidos/Q01/src/br/edu/util/Verifica.java
@@ -0,0 +1,17 @@
+package br.edu.util;
+
+public class Verifica {
+
+	public static int verifica (double num){
+		
+		int verifica;
+		if (num>=0) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+
+	}
+
+}
